6edc06ad67d74ebda3c779c1b51a0bd08bd20455
nepp-plus <nepp.plus@gmail.com>
2021-12-31 12:01:59 +0900

    각 페이지별 게시글 불러내는 쿼리 수행 함수 작성

--- db_handler.py
+++ db_handler.py
@@ -29,3 +29,28 @@ def get_user_list():
     result = cursor.fetchall()
     # API => 원하는 쿼리의 수행 결과를, 가공해서 화면단에서 사용할 수 있게 전달해주는 역할.
     return result
+
+# 페이지에 맞는 게시글 목록 가져오기
+
+def get_posts(page):
+    # 1페이지당 5개의 글을 보여준다.
+    
+    # 1페이지 : 0개의 글 패스, 그 다음 5개. (1~5)
+    # 2페이지 : 5개 패스, 그 다음 5개.
+    # 3페이지 : 10개의 글 패스, 그 다음 5개의 글 보여주기. (11~15)
+    # 4페이지? : 15개 패스, 5개.
+    
+    # ORDER BY / LIMIT 쿼리 활용.
+    # LIMIT 건너뛸갯수, 보여줄 갯수 활용.
+    
+    # 몇페이지냐에 따른 -> 건너뛸 갯수는 몇개?  쿼리 작성가능.
+    offset = (page - 1) * 5
+    
+    sql = f"SELECT * FROM posts AS p ORDER BY p.created_at DESC LIMIT {offset}, 5"
+    
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    
+    print(result)
+    
+    return result
