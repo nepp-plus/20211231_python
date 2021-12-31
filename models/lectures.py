e0a26f9f313167bc41ec784c12144d8a0dee9864
nepp-plus <nepp.plus@gmail.com>
2021-12-31 16:17:40 +0900

    round 기능으로 파이썬에서 소수점 1자리 반올림 처리.

--- models/lectures.py
+++ models/lectures.py
@@ -1,4 +1,6 @@
 class Lectures:
     def __init__(self, data_dict):
         self.name = data_dict['name']
-        self.avg_score = data_dict['avg_score']
+        
+        # 평균점수 : DB에서 긴 소수점이 와도 => 파이썬에서 소수점 1자리로만 넣어주자.
+        self.avg_score = round(data_dict['avg_score'], 1)
